b3463a43f9bdf9ac6158cf692c018e42d437eb31
violaceous <bodhisnatcher@gmail.com>
2014-01-16 10:03:30 -0500

    Added book object. Checking in so I have a fallback point in case I smash everything

--- public/js/slice.js
+++ public/js/slice.js
@@ -1,4 +1,11 @@
-
+function rgbToHex(R,G,B) {return toHex(R)+toHex(G)+toHex(B)}
+function toHex(n) {
+ n = parseInt(n,10);
+ if (isNaN(n)) return "00";
+ n = Math.max(0,Math.min(n,255));
+ return "0123456789ABCDEF".charAt((n-n%16)/16)
+      + "0123456789ABCDEF".charAt(n%16);
+}
 // pixastic stuff
 /*
 var edges = new Image();
@@ -13,10 +20,103 @@ document.body.appendChild(edges);
 edges.src = "img/books1small.jpg";
 */
 
+function Book() {
+    var image = null;
+    var dominantColor = null;
+    var secondaryColor = null;
+    var width = null;
+
+    this.setImage = function(image) {
+	this.image = image;
+    };
+
+    this.getImage = function() {
+	return this.image;
+    };
+
+    this.setDominantColor = function(color) {
+	this.dominantColor = color;
+    };
+
+    this.getDominantColor = function() {
+	return this.dominantColor;
+    };
+
+    this.setSecondaryColor = function(color) {
+	this.secondaryColor = color;
+    };
+
+    this.getSecondaryColor = function() {
+	return this.secondaryColor;
+    };
+
+    this.setWidth = function(width) {
+	this.width = width;
+    };
+
+    this.getWidth = function() {
+	return this.width;
+    }
+}
+
+var bookArray = [];
 var rectangle = [];
+var colorThief = new ColorThief();
+
+function drawBooks(bookArray) {
+    var cur_offset = 0;
+    sel_ctx.clearRect(0, 0, sel_ctx.canvas.width, sel_ctx.canvas.height);
+    for(var i = 0; i < bookArray.length; i++)
+    {
+	var book = bookArray[i];
+	sel_ctx.drawImage(book.getImage(), 0 + cur_offset, 0)
+	cur_offset += book.getWidth();
+    }
+}
+
+// selected books aren't always vertical or selected in the same order
+// this method takes any 4 points and returns the most left and top value
+// for one point and the most right and lowest value for the other
+function getCorners(rectangle) {
+    var right = 0;
+    var left = 100000;
+    var bottom = 0;
+    var top = 100000;
+    
+    for(var i = 0; i < rectangle.length; i++) {
+	curPoint = rectangle[i];
+	if(curPoint[0] > right) {
+	    right = curPoint[0];
+	} else if(curPoint[0] < left) {
+	    left = curPoint[0];
+	}
+	if(curPoint[1] > bottom) {
+	    bottom = curPoint[1];
+	} else if(curPoint[1] < top) {
+	    top = curPoint[1];
+	}
+    }
+    return [[left, top],[right,bottom]];
+} 
 
 function sliceImage(rectangle) {
-    sel_ctx.drawImage(canvas, rectangle[0][0], rectangle[0][1], rectangle[2][0] - rectangle[0][0], rectangle[2][1] - rectangle[0][1], 0, 0,  rectangle[2][0] - rectangle[0][0], rectangle[2][1] - rectangle[0][1]);
+    var tempCanvas = document.createElement('canvas');
+    tempCtx = tempCanvas.getContext('2d')
+    var corners = getCorners(rectangle);
+    tempCtx.canvas.height = rectangle[2][1] - rectangle[0][1];
+    tempCtx.drawImage(canvas, rectangle[0][0], rectangle[0][1], rectangle[2][0] - rectangle[0][0], rectangle[2][1] - rectangle[0][1], 0, 0,  rectangle[2][0] - rectangle[0][0], rectangle[2][1] - rectangle[0][1]);
+    var image = document.createElement('img');
+    image.src = tempCanvas.toDataURL();
+    image.width = tempCtx.canvas.width;
+    image.height = tempCtx.canvas.height;
+    var palette = colorThief.getPalette(image, 2, 5);
+    newBook = new Book();
+    newBook.setImage(image);
+    newBook.setWidth(tempCtx.canvas.width);
+    newBook.setDominantColor(rgbToHex(palette[0][0], palette[0],[1], palette[0],[2]));
+    newBook.setSecondaryColor(rgbToHex(palette[1][0], palette[1],[1], palette[1],[2]));
+    bookArray.push(newBook);
+    drawBooks(bookArray);
 }
 
 $("#the_canvas").ready(function() {
